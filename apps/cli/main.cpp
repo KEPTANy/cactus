ab85f2d2f97be130ae29699d586fa7f61cc28c9a
Vanzha Kirill <vanzha.kirill@gmail.com>
2025-08-09 22:36:21 +0300

    Pretty-print decoded bencode values

--- apps/cli/main.cpp
+++ apps/cli/main.cpp
@@ -1,7 +1,39 @@
 #include <cstdlib>
+#include <iostream>
 
 #include "core/bencode.h"
 
+using namespace bencode;
+
+void print(Entry &val) {
+  switch (val.type()) {
+  case Entry::INT: std::cout << val.as_int(); break;
+  case Entry::STR: std::cout << '\"' << val.as_str() << '\"'; break;
+  case Entry::LIST:
+    std::cout << "[\n";
+    for (auto &e : val.as_list()) {
+      print(e);
+      std::cout << ",\n";
+    }
+    std::cout << ']';
+    break;
+  case Entry::DICT:
+    std::cout << "{\n";
+    for (auto &[k, v] : val.as_dict()) {
+      std::cout << '\"' << k << "\": ";
+      print(v);
+      std::cout << ",\n";
+    }
+    std::cout << '}';
+    break;
+  }
+}
+
 int main() {
+  std::string benc;
+  std::cin >> benc;
+
+  Entry e{decode(benc)};
+  print(e);
   return EXIT_SUCCESS;
 }
